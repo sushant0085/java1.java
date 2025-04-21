c49b8013f8622911a2deb34b2afa15e0cc8eaad3
sushant0085 <sushantak312@gmail.com>
2025-04-22 00:13:33 +0530

    Add files via upload

---
+++ workspace.java
@@ -0,0 +1,19 @@
+public class workspace {
+    public static void main(String[] args) {
+        boolean isRaining = true;
+        boolean isSunny = false; 
+
+        if (isRaining == true) {  
+            System.out.println("Not Going today");
+        }
+        else if (isSunny == true) {  
+            System.out.println("Beach Day");    
+        }
+        else {
+            System.out.println("Just staying home.");
+        }
+    }
+
+
+    
+}
